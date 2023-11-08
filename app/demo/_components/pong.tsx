85a63f55afb474538c2e57c5a04092daf6751b8d
ascappa <andreascappaticci24@gmail.com>
2023-11-08 10:09:39 +0100

    Replace kaboom globals with namespace

--- app/demo/_components/pong.tsx
+++ app/demo/_components/pong.tsx
@@ -6,102 +6,82 @@ export function Pong() {
   const canvasRef = useRef(null);
   const [count, setCount] = useState(0)
   useEffect(() => {
-    const {
-      easings,
-      area,
-      body,
-      tween,
-      add,
-      rect,
-      pos,
-      color,
-      onKeyDown,
-      onClick,
-      circle,
-      onUpdate,
-      width,
-      height,
-      mousePos,
-      onTouchMove,
-      dt,
-      onMouseMove,
-      loadJSON,
-      sprite,
-      loadSprite,
-      scale
-    } = kaboom({
+    const k = kaboom({
       canvas: canvasRef.current!,
       background: [0, 0, 255],
-      width: 700,
-      height: 500,
+      width: 500,
+      height: 700,
+      stretch: true,
       global: true,
     });
-    const playerBar = add([
-      rect(100, 10, { radius: 3 }),
-      pos(40, height() - 30),
-      color(0, 255, 0),
-      area(),
+    const playerBar = k.add([
+      k.rect(100, 10, { radius: 3 }),
+      k.pos(40, k.height() - 30),
+      k.color(0, 255, 0),
+      k.area(),
       "playerBar"
     ]);
-    const opponentBar = add([
-      rect(100, 10, { radius: 3 }),
-      pos(50, 20),
-      color(0, 255, 0),
+    console.log(k.height())
+    const opponentBar = k.add([
+      k.rect(100, 10, { radius: 3 }),
+      k.pos(50, 20),
+      k.color(0, 255, 0),
+      k.area()
     ]);
-    loadSprite("infinityTrain", "/sprite.png")
-    add([sprite("infinityTrain"), pos(50, 50), scale(0.1)])
-    const ball = add([circle(10), pos(145, 295), color(255, 255, 255), area()]);
+    k.loadSprite("infinityTrain", "/sprite.png")
+    k.add([k.sprite("infinityTrain"), k.pos(50, 50), k.scale(0.1)])
+    const ball = k.add([k.circle(10), k.pos(145, 295), k.color(255, 255, 255), k.area()]);
     let ballXDirection = -1;
     let ballYDirection = 1;
     const ballSpeed = 250
-    onKeyDown("right", () => {
+    k.onKeyDown("right", () => {
       playerBar.moveBy(2, 0);
     });
-    onKeyDown("left", () => {
+    k.onKeyDown("left", () => {
       playerBar.moveBy(-2, 0);
     });
     
-    onUpdate(() => {  
-      ball.moveBy(ballXDirection * ballSpeed * dt(), ballYDirection * ballSpeed * dt());
+    k.onUpdate(() => {  
+      ball.moveBy(ballXDirection * ballSpeed * k.dt(), ballYDirection * ballSpeed * k.dt());
       if (
         ball.pos.x - ball.radius <= 0 ||
-        ball.pos.x + ball.radius >= width()
+        ball.pos.x + ball.radius >= k.width()
       ) {
         ballXDirection *= -1;
       }
       if (
         ball.pos.y - ball.radius <= 0 ||
-        ball.pos.y + ball.radius >= height()
+        ball.pos.y + ball.radius >= k.height()
       ) {
         ballYDirection *= -1;
       }
     });
-    onClick(() => {
-      const { x } = mousePos();
-      tween(
+    k.onClick(() => {
+      const { x } = k.mousePos();
+      k.tween(
         playerBar.pos.x,
         x - 50,
         0.3,
         (p) => (playerBar.pos.x = p),
-        easings.easeOutSine,
+        k.easings.easeOutSine,
       );
     });
-    onTouchMove(() => {
-      const { x } = mousePos();
-      tween(
+    k.onTouchMove(() => {
+      const { x } = k.mousePos();
+      k.tween(
         playerBar.pos.x,
         x - 50,
         0.1,
         (p) => (playerBar.pos.x = p),
-        easings.easeOutSine,
+        k.easings.easeOutSine,
       );
     })
     ball.onCollide(() => {
       ballYDirection *= -1
     })
-    onMouseMove(() => {
-      playerBar.moveTo(mousePos().x - playerBar.width / 2, playerBar.pos.y)
+    k.onMouseMove(() => {
+      playerBar.moveTo(k.mousePos().x - playerBar.width / 2, playerBar.pos.y)
     })
   }, []);
-  return <><div onClick={() => setCount(count + 1)}>{count}</div><canvas ref={canvasRef}></canvas></>;
+  return <><div onClick={() => setCount(count + 1)}>{count}</div><div><canvas ref={canvasRef}></canvas></div></>;
 }
